2cdb0346aed7b340157a4c7b6d867427c3c15ee3
rauno valling <rvalling@tktk.ee>
2021-09-16 23:06:31 +0300

    Control Flow

---
+++ UserAge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm loob sünniaastat konsoolis
+            //"oled sündinud aastal {yearOfBirth}"
+
+            Console.WriteLine("Palun, sisesta oma vanus: ");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            int yearOfBirth = 2021 - userAge;
+
+           
+
+            Console.WriteLine($"oled sündinud aastal {yearOfBirth}");
+
+
+        }
+    }
+}
---
+++ UserData/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UserData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //programm küsib kasutaja sünniaastat;
+            //programm arvutab kasutaja vanust;
+            //programm kuvab kasutaja vanust konsoolis;
+
+            Console.WriteLine("Palun, sisesta oma sünniaasta: ");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2021 - yearOfBirth;
+
+            //Console.WriteLine("oled" + userAge + "aastat vana");
+
+            //string interpolation
+
+            Console.WriteLine($"Oled {userAge} aastat vana.");
+
+
+        }
+    }
+}
